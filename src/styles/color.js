55a898310475ea10437bdb2de3e8cd069d8da936
양인서 <113487989+sheepyis@users.noreply.github.com>
2024-01-27 00:38:32 +0900

    ✨ 크루 포인트 순위 추가
    화살표 클릭 시 6개씩 출력하는 것의 오류,
    1위 크루 포인트 점수 색 변화 오류
    
    해결할 예정

--- src/styles/color.js
+++ src/styles/color.js
@@ -5,7 +5,9 @@ const colors = {
     markerDiv: "#F9F9F9",
     crewGray: "#17191A",
     crewGray2: "#464A4D",
+    crewGray3: "#9E9E9E",
     lightGray: "#EEEEEE",
+    crewPink: "#FFF6F7",
     white: "#FFFFFF",
     black: "#000000"
 };
