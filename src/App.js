3bba265d10f46788a45824e30eeac40cc1f06aa6
nunomi0 <ykys1@naver.com>
2024-02-05 02:44:30 +0900

    header link 수정 (comnmunity)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
 import GlobalStyle from './styles/globalStyle';
+
 import Header from './components/Header/Header';
 import HeaderMenu from "./components/HeaderMenu/HeaderMenu";
 import Footer from './components/Footer/Footer';
 
 import Home from './pages/Home/Home';
+
 import Blood from './pages/Blood/Blood';
+import SinglePost from './pages/Blood/BloodSinglePost/SinglePost';
+import BloodWrite from './pages/Blood/BloodWrite/BloodWrite';
+
 import Community from './pages/Community/Community';
+import CommunityWrite from './pages/Community/CommunityWrite/CommunityWrite';
+
 import Crew from './pages/Crew/Crew';
 import CrewUpload from './pages/Crew/CrewUpload';
 import CrewDetail from './pages/Crew/CrewDetail';
+
 import Live from './pages/Live/Live';
-import NotFound from './pages/NotFound/NotFound';
-import CommunityWrite from './pages/Community/CommunityWrite/CommunityWrite';
-import BloodWrite from './pages/Blood/BloodWrite/BloodWrite';
-import SinglePost from './pages/Blood/BloodSinglePost/SinglePost';
 
 import MyPage from './pages/MyPage/MyPage';
+
+import NotFound from './pages/NotFound/NotFound';
+
 
 function App() {
   const [hoveredComponent, setHoveredComponent] = useState(null);
@@ -42,9 +49,11 @@ function App() {
         />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="blood" element={<Blood />} />
-          <Route path="Blood/BloodSinglePost/SinglePost" element={<SinglePost />} />
-          <Route path="/community" element={<BloodWrite />} />
+          <Route path="/blood" element={<Blood />} />
+          <Route path="/blood/bloodWrite/bloodwrite" element={<BloodWrite />} />
+          <Route path="/blood/bloodsinglepost/singlepost" element={<SinglePost />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/communityWrite" element={<CommunityWrite />} />
           <Route path="/crew" element={<Crew />} />
           <Route path="/crewupload" element={<CrewUpload />} />
           <Route path="/crewdetail/:id" element={<CrewDetail />} />
