f7adfe53d6125f0ed1e451d6fdd31404cebc7997
umhye1 <lucy021206@gmail.com>
2024-02-06 02:45:38 +0900

    로그인 페이지 생성

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../../styles/color";
+import {Link} from "react-router-dom";
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    padding-top: 2vw;
+    margin-bottom: 4vw;
+`
+const SideBar = styled.div`
+    width: 17%;
+    padding-left: 2.5%;
+`
+const MainConationer =styled.div`
+    width: 67%;
+`
+
+const Breadcrums = styled.div`
+    display: flex;
+    gap: 0.5vw;
+    padding: 0.9375vw 0.0000vw 0.9375vw 0.2083vw;
+`
+const BreadcrumsP = styled.div`
+    font-weight: 500;
+    font-size: 0.6vw;
+    color: ${colors.crewGray2};
+    cursor: pointer;
+`
+const Title = styled.div`
+    color: var(--Gray-Gray-900, #17191A);
+    font-family: Pretendard;
+    font-size: 1.2500vw;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 2.1875vw; /* 175% */
+`
+const LoginContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 0.4167vw;
+`
+const LoginBox = styled.div`
+    width: 24.4792vw;
+    height: 22.2917vw;
+    flex-shrink: 0;
+    border-radius: 0.2604vw;
+    border: 0.0521vw solid var(--Gray-Gray-300, #D1D1D1);
+`
+const InputBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .id{
+        display: flex;
+        width: 21.3542vw;
+        margin-top: 4.8438vw;
+        padding: 0.5208vw;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        gap: 0.5208vw;
+        border-bottom: 0.0521vw solid var(--Gray-Gray-500, #9E9E9E);
+    }
+    .password{
+        display: flex;
+        width: 21.3542vw;
+        margin: 1.0417vw 0.0000vw 4vw 0.0000vw;
+        padding: 0.5208vw;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        gap: 0.5208vw;
+        border-bottom: 0.0521vw solid var(--Gray-Gray-500, #9E9E9E);
+    }
+`
+const LoginButton =styled.button`
+    width: 19.3750vw;
+    height: 2.6042vw;
+    border-radius: 0.2604vw;
+    background: var(--Primary-Red-900, #E95458);
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 0.9375vw;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 1vw;
+    margin-top:1.75vw;
+    .login{
+        color: var(--black-white-white-1000, #FFF);
+    }
+`
+
+const FindContainer = styled.div`
+    display:flex;
+    flex-direction: space;
+    justify-content: space-between;
+    margin: 1.0417vw 1.5625vw 1.5625vw 1.5625vw;
+`
+
+const FindP= styled.p`
+    color: var(--Gray-Gray-700, #464A4D);
+    font-family: Pretendard;
+    font-size: 0.7813vw;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%; 
+    letter-spacing: -0.0187vw;`
+
+
+const Login = () =>{
+
+
+    return (
+        <Container>
+            <SideBar/>
+            <MainConationer>
+                <Breadcrums>
+                    <BreadcrumsP>홈</BreadcrumsP>
+                    <BreadcrumsP>{">"}</BreadcrumsP>
+                    <BreadcrumsP>로그인</BreadcrumsP>
+                </Breadcrums>
+                <Title>로그인</Title>
+                <LoginContainer>
+                    <LoginBox>
+                        <InputBox>
+                            <input className="id" type="text" placeholder="ID"/>
+                            <input className="password" type="password" placeholder="PASSWORD"/>
+                            <LoginButton><input className="login" type="submit" value="로그인"/></LoginButton>
+                        </InputBox>
+                        
+                        <FindContainer>
+                            <Link to ="/findPassword">
+                                <FindP>비밀번호찾기</FindP>
+                            </Link>
+                            <Link to="/join">
+                                <FindP>회원가입</FindP>
+                            </Link>
+                        </FindContainer>
+                    </LoginBox>
+                </LoginContainer>
+                
+
+            </MainConationer>
+        </Container>
+    )
+}
+
+export default Login;
+
+
