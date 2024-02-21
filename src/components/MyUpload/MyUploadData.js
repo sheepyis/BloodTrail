e838a2f7c652fa09cbf4edd64ad335139efaa328
sheepyis <zx9086@naver.com>
2024-02-21 13:22:45 +0900

    🚑 더미 데이터 추가

---
+++ src/components/MyUpload/MyUploadData.js
@@ -0,0 +1,9 @@
+const MyUploadData = [
+        { id: 1, title: "수유 헌혈의 집 후기!", name: "나", postday: "2024-01-30", view: 96, like: 52 },
+        { id: 2, title: "빈혈이 있는데 헌혈 가능한 지 봐주실래요?", name: "나", postday: "2024-02-11", view: 55, like: 26 },
+        { id: 3, title: "모두 헌혈해서 좋은 일에 동참해요!!", name: "나", postday: "2024-02-19", view: 112, like: 48 },
+        { id: 4, title: "지정헌혈이 무엇인지 알고 싶어요.", name: "나", postday: "2024-02-21", view: 23, like: 57 },
+        { id: 5, title: "지금 당산역쪽인데 근처에 헌혈의 집 있을까요?", name: "나", postday: "2024-02-21", view: 11, like: 3 },
+    ];
+
+export default MyUploadData;
